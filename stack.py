786cf5840d1e4dc479eb8019f1ff13cc40d827c3
dev-ferrario <51686616+dev-ferrario@users.noreply.github.com>
2019-07-09 15:09:45 -0400

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,17 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, data):
+        self.items.append(data)
+
+    def pop(self):
+        return self.items.pop()
+
+    def top(self):
+        return self.items[len(self.items) - 1]
+
+
