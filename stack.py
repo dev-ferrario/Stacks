a40dca0fb954294f63b944e2f2e5a5da3bebdbbb
dev-ferrario <51686616+dev-ferrario@users.noreply.github.com>
2019-07-10 09:04:05 -0400

    Added reverse methods as well as a print method

--- stack.py
+++ stack.py
@@ -16,4 +16,21 @@ class Stack:
     def top(self):
         return self.items[len(self.items) - 1]
 
+    def reverse1(self):
+        result = self.items.copy()
+        lo = 0
+        hi = len(self.items) - 1
+        while lo < hi:
+            result[lo], result[hi] = result[hi], result[lo]
+            lo += 1
+            hi -= 1
+        return result
+
+    def reverse2(self):
+        result = self.items.copy()
+        result.reverse()
+        return result
+
+    def print(self):
+        print(self.items)
 
