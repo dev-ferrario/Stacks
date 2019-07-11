ae28b161748b8f6a16327c995aaba665d7d4f273
Brianna Peticca <bripeticca@gmail.com>
2019-07-11 13:34:03 -0700

    Update stack.py

--- stack.py
+++ stack.py
@@ -8,7 +8,7 @@ class Stack:
     def is_empty(self):
         return self.items == []
 
-    def push(self, hehe):
+    def push(self, data):
         self.items.append(data)
         if not self.maxVal or data >= self.maxVal[len(self.maxVal) - 1]:
             self.maxVal.append(data)
